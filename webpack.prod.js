32c2c664818d8a1b1d72e292cf2b21eb534f04ae
Jinjin Li <li@fundsaccess.com>
2019-12-15 22:00:19 +0100

    added build for showcase

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const output = 'docs';
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/lib/ng-chiptan.js'),
+  entry: path.resolve(__dirname, 'src/app/app.js'),
   output: {
-    filename: 'ng-chiptan.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, output),
   },
   plugins: [
     new CopyPlugin([
-      { from: path.resolve(__dirname, 'src/lib/package.json'), to: path.resolve(__dirname, 'dist') },
-      { from: path.resolve(__dirname, 'README.md'), to: path.resolve(__dirname, 'dist') },
+      { from: path.resolve(__dirname, 'src/index.html'), to: path.resolve(__dirname, output) },
+      { from: path.resolve(__dirname, 'src/favicon.ico'), to: path.resolve(__dirname, output) }
     ]),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin()
   ]
 };
