e3adb4117aa3f97eac324d430bde0fa5c73e3267
Ali Elderov <elderov@fundsaccess.com>
2020-06-14 23:59:16 +0200

    refactor: update webpack.dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app/app.js',
-  watch: true,
-  devServer: {
-    contentBase: path.join(__dirname, 'src')
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'src'),
-  },
+    entry: './src/app/app.js',
+    watch: true,
+    devServer: {
+        contentBase: path.join(__dirname, 'src')
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'src')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                exclude: path.resolve('./src/index.html'),
+                use: ['html-loader']
+            },
+            {
+                test: /\.js$/i,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 };
