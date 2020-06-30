ba0f5d78bc6e1c463350b791107c2dcf2bef285f
Jinjin Li <li@fundsaccess.com>
2020-06-30 21:37:41 +0200

    chore: use babel-loader to transpile es6 to es5

--- webpack.lib.js
+++ webpack.lib.js
@@ -8,6 +8,20 @@ module.exports = {
     filename: 'ng-chiptan.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new CopyPlugin([
       { from: path.resolve(__dirname, 'src/lib/package.json'), to: path.resolve(__dirname, 'dist') },
