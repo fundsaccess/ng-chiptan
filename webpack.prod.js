c0833021520ac34f4b129ed065990dc36ac2cd84
Jinjin Li <li@fundsaccess.com>
2020-06-30 21:48:46 +0200

    chore: use babel-loader also for showcase

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,6 +9,20 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, output),
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new CopyPlugin([
       { from: path.resolve(__dirname, 'src/index.html'), to: path.resolve(__dirname, output) },
